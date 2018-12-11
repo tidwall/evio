6dd9d0626bf48e93bf113c7742f7832da8c7ce95
Aspirin2d <aspirin2d@outlook.com>
2018-12-11 10:52:48 +0800

    fix auto-close bug, when using evio_unix.

--- evio_unix.go
+++ evio_unix.go
@@ -422,12 +422,18 @@ func loopWake(s *server, l *loop, c *conn) error {
 func loopRead(s *server, l *loop, c *conn) error {
 	var in []byte
 	n, err := syscall.Read(c.fd, l.packet)
-	if n == 0 || err != nil {
+	if err != nil {
 		if err == syscall.EAGAIN {
 			return nil
 		}
 		return loopCloseConn(s, l, c, err)
 	}
+
+	// when data is empty return not close
+	if n == 0 {
+		return nil
+	}
+
 	in = l.packet[:n]
 	if !c.reuse {
 		in = append([]byte{}, in...)
