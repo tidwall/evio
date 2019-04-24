e2353136faa6cc81eafc4ea63ade3621e901793b
Andy Pan <panjf2000@gmail.com>
2019-04-24 21:32:41 +0800

    Make sure it only signalShutdown once

--- evio_unix.go
+++ evio_unix.go
@@ -443,9 +443,6 @@ func loopRead(s *server, l *loop, c *conn) error {
 	}
 	if len(c.out) != 0 || c.action != None {
 		l.poll.ModReadWrite(c.fd)
-		if len(c.out) > 0 && None == c.action {
-			return loopWrite(s, l, c)
-		}
 	}
 	return nil
 }
@@ -487,6 +484,47 @@ func (c *detachedConn) Write(p []byte) (n int, err error) {
 		p = p[nn:]
 	}
 	return n, nil
+}
+
+func (ln *listener) close() {
+	if ln.fd != 0 {
+		syscall.Close(ln.fd)
+	}
+	if ln.f != nil {
+		ln.f.Close()
+	}
+	if ln.ln != nil {
+		ln.ln.Close()
+	}
+	if ln.pconn != nil {
+		ln.pconn.Close()
+	}
+	if ln.network == "unix" {
+		os.RemoveAll(ln.addr)
+	}
+}
+
+// system takes the net listener and detaches it from it's parent
+// event loop, grabs the file descriptor, and makes it non-blocking.
+func (ln *listener) system() error {
+	var err error
+	switch netln := ln.ln.(type) {
+	case nil:
+		switch pconn := ln.pconn.(type) {
+		case *net.UDPConn:
+			ln.f, err = pconn.File()
+		}
+	case *net.TCPListener:
+		ln.f, err = netln.File()
+	case *net.UnixListener:
+		ln.f, err = netln.File()
+	}
+	if err != nil {
+		ln.close()
+		return err
+	}
+	ln.fd = int(ln.f.Fd())
+	return syscall.SetNonblock(ln.fd, true)
 }
 
 func reuseportListenPacket(proto, addr string) (l net.PacketConn, err error) {
